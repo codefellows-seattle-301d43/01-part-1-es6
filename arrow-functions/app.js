68dfc6403e2d7c7ec97bd4f46d8d63400e75618a
richjimdev <richjimdev@gmail.com>
2018-09-04 11:30:11 -0700

    step 11

--- arrow-functions/app.js
+++ arrow-functions/app.js
@@ -172,17 +172,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-// 
+// It's the Student object
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// It's the window Object
 // 3. Explain why "this" is different when an arrow function is used.
-// 
+// 'this' gets moved up to whatever is enclosing the arrow function, if there is nothing enclosing it, it moved up to the window object
