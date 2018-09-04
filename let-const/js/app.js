d64e0e2937138e69921f8917788ea602be2a58f3
alstof <alstof@gmail.com>
2018-09-04 09:49:39 -0700

    Fix bug with function-scoped let in global scope

--- let-const/js/app.js
+++ let-const/js/app.js
@@ -1,6 +1,27 @@
 'use strict';
 
-let names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+let names = [
+  'bag',
+  'banana',
+  'bathroom',
+  'boots',
+  'breakfast',
+  'bubblegum',
+  'chair',
+  'cthulhu',
+  'dog-duck',
+  'dragon',
+  'pen',
+  'pet-sweep',
+  'scissors',
+  'shark',
+  'sweep',
+  'tauntaun',
+  'unicorn',
+  'usb',
+  'water-can',
+  'wine-glass'
+];
 
 let leftImage = document.getElementById('left');
 let centerImage = document.getElementById('center');
@@ -28,19 +49,18 @@ function makeRandom() {
   return Math.floor(Math.random() * names.length);
 }
 
-function displayPics(){
-  while(viewed.length < 6){
+function displayPics() {
+  while (viewed.length < 6) {
     let rando = makeRandom();
-    while(!viewed.includes(rando)){
+    while (!viewed.includes(rando)) {
       viewed.push(rando);
     }
   }
-  console.log(rando);
   // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the letiable declaration from `let to `let`.
-  // PUT YOUR RESPONSE IN THIS COMMENT
+  // The variable rando declared using 'let' inside a code block and can only be accessed inside the same block.
   console.log(viewed);
 
-  for (let i = 0; i < 3; i++){
+  for (let i = 0; i < 3; i++) {
     let temp = viewed.shift();
     pics[i].src = allProducts[temp].path;
     pics[i].id = allProducts[temp].name;
@@ -53,16 +73,23 @@ function handleClick(event) {
     return alert('Be sure to click directly on an image!!');
   }
   totalClicks += 1;
-  if(totalClicks > 24) {
+  if (totalClicks > 24) {
     container.removeEventListener('click', handleClick);
     container.style.display = 'none';
     showList();
     makeChart();
   }
-  for(let i = 0; i < names.length; i++){
-    if(event.target.id === allProducts[i].name) {
+  for (let i = 0; i < names.length; i++) {
+    if (event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
-      console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+      console.log(
+        event.target.id +
+          ' has ' +
+          allProducts[i].votes +
+          ' votes in ' +
+          allProducts[i].views +
+          ' views'
+      );
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -71,45 +98,56 @@ function handleClick(event) {
 }
 
 function showList() {
-  for(let i = 0; i < allProducts.length; i++) {
+  for (let i = 0; i < allProducts.length; i++) {
     let liEl = document.createElement('li');
-    liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+    liEl.textContent =
+      allProducts[i].name +
+      ' has ' +
+      allProducts[i].votes +
+      ' votes in ' +
+      allProducts[i].views +
+      ' views';
     list.appendChild(liEl);
   }
 }
 
-function makeChartData(){
-  allProducts.forEach(function(product){
+function makeChartData() {
+  allProducts.forEach(function(product) {
     labels.push(product.name);
     votes.push(product.votes);
     views.push(product.views);
   });
 }
 
-function makeChart(){
+function makeChart() {
   makeChartData();
   let ctx = document.getElementById('chartypants').getContext('2d');
-  new Chart(ctx, { //eslint-disable-line
+  new Chart(ctx, {
+    //eslint-disable-line
     type: 'bar',
     data: {
       labels: labels,
-      datasets: [{
-        label: 'total votes',
-        backgroundColor: 'gold',
-        borderColor: '#214',
-        data: votes,
-      }]
+      datasets: [
+        {
+          label: 'total votes',
+          backgroundColor: 'gold',
+          borderColor: '#214',
+          data: votes
+        }
+      ]
     },
     options: {
       responsive: false,
       scales: {
-        yAxes: [{
-          ticks: {
-            max: 20,
-            min: 0,
-            stepSize: 1
+        yAxes: [
+          {
+            ticks: {
+              max: 20,
+              min: 0,
+              stepSize: 1
+            }
           }
-        }]
+        ]
       }
     }
   });
@@ -118,17 +156,19 @@ function makeChart(){
 
 container.addEventListener('click', handleClick);
 
-document.getElementById('bus').addEventListener('click', function(){
+document.getElementById('bus').addEventListener('click', function() {
   localStorage.removeItem('busmall');
   console.log('Local storage was cleared!');
 });
 
-if(localStorage.busmall){
+if (localStorage.busmall) {
   console.log('Local storage data exists');
   allProducts = JSON.parse(localStorage.busmall);
 } else {
-  console.log('There is no local storage data; initialize app by creating instances');
-  for(let i = 0; i < names.length; i++) {
+  console.log(
+    'There is no local storage data; initialize app by creating instances'
+  );
+  for (let i = 0; i < names.length; i++) {
     new Product(names[i]);
   }
 }
